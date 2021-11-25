4134ecde8dd71a001ec8422b883f8892795fc4d9
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-26 00:19:09 +0700

    Add files via upload

---
+++ cari nama.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string kelas[5] = {"zayn","harry","niall","louis","liam"};
+	string cari;
+
+	bool cek = false;
+	
+	cout << "nama yang ingin dicari : ";
+	getline(cin, cari);
+	
+	for(int i=0;i<5;i++){
+		if(cari==kelas[i]){ 
+			cout << cari << " adalah member one direction";
+			cek = true;
+		}
+	}
+	
+	if(cek==false){
+		cout << cari << " bukan member one direction";
+	}
+}
